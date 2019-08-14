fd1f60f7a70f1cfe284218c8917789fa16a4b8ef
Michael J. Curry <curry@cs.umd.edu>
2019-08-14 09:55:10 -0400

    closer to final formatting

--- matching2.py
+++ matching2.py
@@ -221,52 +221,59 @@ def opt_score(history, max_t, e_weights_type):
                         name=f'x_{i}_{j}_{t}',
                         )
     model.update()
-    print('created all variables')
 
     # constraint: each node matched once
     match_once_constraints = []
     for i in range(n_nodes):
         match_once_constraints.append(model.addConstr(gp.quicksum(x[i,j,t] for  j in range(n_nodes) for t in range(max_t)) <= 1))
         
-    print('added match once constraints')
 
     # constraint: for each node, zero before it arrives and after it departs
     arrive_depart_constraints = []
     for i, node_info in enumerate(history):
         t_arrive = node_info[1]
         t_depart = node_info[2]
-        for j in range(n_nodes):
-            for t in range(0, t_arrive):
-                model.addConstr(x[i,j,t] == 0)
-            for t in range(t_depart, max_t): # or is it t_depart + 1???? this is important!
-                model.addConstr(x[i,j,t] == 0)
+        #for j in range(n_nodes):
+            #for t in range(0, t_arrive):
+                #model.addConstr(x[i,j,t] == 0)
+            #for t in range(t_depart, max_t): # or is it t_depart + 1???? this is important!
+                #model.addConstr(x[i,j,t] == 0)
+        arrive_depart_constraints.append(model.addConstr(gp.quicksum(x[i,j,t] for j in range(n_nodes) for t in range(0, t_arrive)) == 0))
+        arrive_depart_constraints.append(model.addConstr(gp.quicksum(x[i,j,t] for j in range(n_nodes) for t in range(t_depart, max_t)) == 0))
     
-    print('added arrive/depart constraints')
-
         
 
     # we don't need an additional binaryness constraint because of variable type
 
     # create objective while computing weights for each edge
     obj = gp.LinExpr()
+    varwise_edge_weights = {}
     for i, node_info_i in enumerate(history):
         for j, node_info_j in enumerate(history):
             random_jitter = random.random() * 1e-5
             i_type = node_info_i[0]
             j_type = node_info_j[0]
+            varwise_edge_weights[i,j] = e_weights_type[i_type, j_type].item()
             edge_weight = -e_weights_type[i_type, j_type] + random_jitter
             for t in range(max_t):
                 obj += x[i,j,t] * edge_weight.item()
     model.setObjective(obj, gp.GRB.MINIMIZE)
 
-    print('succeeded in creating model! Now optimizing...')
 
     model.optimize()
 
 
+    # enumerate nonzero variables and sum with varwise edge weights
 
-    print(model.ObjVal)
-    return model.ObjVal
+    total_positive_obj = 0.0
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            for t in range(max_t):
+                val = x[i,j,t].x
+                if val > 0.0:
+                    total_positive_obj += varwise_edge_weights[i,j]
+
+    return total_positive_obj
 
 def eval_func(list_of_histories, trained_weights, n_rounds = 50, n_epochs=100):
     e_weights_type = toy_e_weights_type()
